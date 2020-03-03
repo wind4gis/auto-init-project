609787ffe5f9eb8b28807994113a7c90925d6856
wind <huangcanfeng@wljs.com>
2020-03-03 20:35:37 +0800

    如果已经存在了目录，不会继续执行后续逻辑

--- cli.js
+++ cli.js
@@ -2,13 +2,13 @@
 /*
  * @Date: 2020-3-3 18:30:09
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-03-03 20:04:37
+ * @LastEditTime: 2020-03-03 20:34:36
  * @Description: 输入文件夹自动初始化对应的js和scss文件
  */
 const { join } = require("path");
 const { writeFile, mkdirSync, existsSync } = require("fs");
 const { from } = require("rxjs");
-const { map, reduce } = require("rxjs/operators");
+const { map, scan } = require("rxjs/operators");
 const getContent = require("./config");
 
 // 读取传入的文件夹参数
@@ -19,7 +19,7 @@ const curFolder = process.cwd();
 from(args)
   .pipe(
     // 返回合并后的目录
-    reduce((acc, cur) => join(acc, cur), curFolder),
+    map(cur => join(curFolder, cur)),
     // 判断指定的目录是否已经存在
     map(folder => {
       if (existsSync(folder)) return "";
@@ -29,7 +29,10 @@ from(args)
   )
   .subscribe(folder => {
     // 根据返回的目录执行具体的写文件操作
-    if (!folder) console.log("folder exists");
+    if (!folder) {
+      console.log("folder exists");
+      return;
+    }
     // 初始化index.js文件
     writeFile(join(folder, "index.js"), getContent(), err => {
       err && console.error(err);
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-03-03 16:47:23
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-03-03 17:33:32
+ * @LastEditTime: 2020-03-03 20:28:21
  * @Description: 默认配置的文本内容
  */
 module.exports = () => {
@@ -9,5 +9,17 @@ module.exports = () => {
   const N = "\n";
   const createDate = date.toLocaleDateString("zh").replace(/\//g, "-");
   const createTime = date.toLocaleTimeString("zh", { hour12: false });
-  return `/*${N} * @Date: ${createDate} ${createTime}${N} * @LastEditors: Huang canfeng${N} * @LastEditTime: ${createDate} ${createTime}${N} * @Description:${N} */${N}`;
+  return `/*${N} * @Date: ${createDate} ${createTime}${
+    N} * @LastEditors: Huang canfeng${
+    N} * @LastEditTime: ${createDate} ${createTime}${
+    N} * @Description:${
+    N} */${
+    N}import React, { memo, useCallback, useRef, useEffect } from "react";${
+    N}import css from "./index.scss";${
+    N}${
+    N}const  = memo(props => {${
+    N}  return ();${
+    N}});${
+    N}export default ;
+    `;
 };
