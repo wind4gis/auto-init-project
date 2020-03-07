2bed482b04a51b66d8b7aef85478c2ef1c8e6c30
wind <huangcanfeng@wljs.com>
2020-03-07 14:58:38 +0800

    新增修改scss文件内容

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 /*
  * @Date: 2020-3-3 18:30:09
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-03-04 10:16:29
+ * @LastEditTime: 2020-03-05 18:30:21
  * @Description: 输入文件夹自动初始化对应的js和scss文件
  */
 const { join, basename } = require("path");
@@ -10,6 +10,8 @@ const { writeFile, mkdirSync, existsSync } = require("fs");
 const { from } = require("rxjs");
 const { map, scan } = require("rxjs/operators");
 const getContent = require("./config");
+
+const { getJsContent, getScssContent } = getContent;
 
 // 读取传入的文件夹参数
 const [, , ...args] = process.argv;
@@ -34,11 +36,11 @@ from(args)
       return;
     }
     // 初始化index.js文件
-    writeFile(join(folder, "index.js"), getContent(basename(folder)), err => {
+    writeFile(join(folder, "index.js"), getJsContent(basename(folder)), err => {
       err && console.error(err);
     });
     // 初始化index.scss文件
-    writeFile(join(folder, "index.scss"), "", err => {
+    writeFile(join(folder, "index.scss"), getScssContent(), err => {
       err && console.error(err);
     });
   });
--- config.js
+++ config.js
@@ -1,28 +1,52 @@
 /*
  * @Date: 2020-03-03 16:47:23
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-03-04 10:25:01
+ * @LastEditTime: 2020-03-05 18:32:47
  * @Description: 默认配置的文本内容
  */
-module.exports = folder => {
-  const date = new Date();
-  const N = "\n";
-  const createDate = date.toLocaleDateString("zh").replace(/\//g, "-");
-  const createTime = date.toLocaleTimeString("zh", { hour12: false });
-  const [firstChar, ...restChar] = folder || "";
-  const name = [String.prototype.toUpperCase.call(firstChar), ...restChar].join("");
+module.exports = {
+  /**
+   * @name: 根据传入的目录名初始化Js文件
+   */
+  getJsContent: folder => {
+    const date = new Date();
+    const N = "\n";
+    const createDate = date.toLocaleDateString("zh").replace(/\//g, "-");
+    const createTime = date.toLocaleTimeString("zh", { hour12: false });
+    const [firstChar, ...restChar] = folder || "";
+    const name = [String.prototype.toUpperCase.call(firstChar), ...restChar].join("");
+  
+    return `/*${N} * @Date: ${createDate} ${createTime}${
+      N} * @LastEditors: Huang canfeng${
+      N} * @LastEditTime: ${createDate} ${createTime}${
+      N} * @Description:${
+      N} */${
+      N}import React, { memo, useCallback, useRef, useEffect } from "react";${
+      N}import css from "./index.scss";${
+      N}${
+      N}const ${name} = memo(props => {${
+      N}  return <></>;${
+      N}});${
+      N}export default ${name};${
+      N}`;
+  },
 
-  return `/*${N} * @Date: ${createDate} ${createTime}${
-    N} * @LastEditors: Huang canfeng${
-    N} * @LastEditTime: ${createDate} ${createTime}${
-    N} * @Description:${
-    N} */${
-    N}import React, { memo, useCallback, useRef, useEffect } from "react";${
-    N}import css from "./index.scss";${
-    N}${
-    N}const ${name} = memo(props => {${
-    N}  return <></>;${
-    N}});${
-    N}export default ${name};
-    `;
-};
+  /**
+   * @name: 初始化Scss文件
+   */  
+  getScssContent: () => {
+    const N = "\n";
+    return `.con {${
+      N}${
+      N}}${
+      N}${
+      N}.img {${
+      N}${
+      N}}${
+      N}${
+      N}.txt {${
+      N}${
+      N}}${
+      N}`;
+  }
+}
