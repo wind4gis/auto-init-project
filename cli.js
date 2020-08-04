6062ce3cc432b602d03014c3cd902fa97ac83855
mid_autumn <mid_autumn@126.com>
2020-08-04 22:33:59 +0800

    新增tsx

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 /*
  * @Date: 2020-3-3 18:30:09
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-03-24 10:37:25
+ * @LastEditTime: 2020-08-04 22:28:29
  * @Description: 输入文件夹自动初始化对应的js和scss文件
  */
 const { join, basename } = require("path");
@@ -17,39 +17,44 @@ const [, , ...args] = process.argv;
 const curFolder = process.cwd();
 
 let folderArgs = [],
-  force = false;
+	force = false,
+	tsx = false;
 
 folderArgs = args.reduce((total, cur) => {
-  if (!~cur.indexOf("-")) {
-    total.push(cur);
-  } else {
-    force = cur === "-f";
-  }
-  return total;
+	if (!~cur.indexOf("-")) {
+		total.push(cur);
+	} else if (cur === "--tsx") {
+		tsx = true;
+	} else if (cur === "-f") {
+		force = true;
+	}
+	return total;
 }, []);
 
 from(folderArgs)
-  .pipe(
-    // 返回合并后的目录
-    map(cur => join(curFolder, cur)),
-    // 判断指定的目录是否已经存在
-    map(folder => {
-      if (existsSync(folder)) return force ? folder : "";
-      mkdirSync(folder);
-      return folder;
-    })
-  )
-  .subscribe(folder => {
-    // 根据返回的目录执行具体的写文件操作
-    if (!folder) {
-      console.log("folder exists");
-      return;
-    }
-
-    // 根据配置文件里指定的文件后缀名以及模板内容，生成对应的文件
-    parserConfig.forEach(({ ext, parser }) => {
-      writeFile(join(folder, `index.${ext}`), parser(basename(folder)), err => {
-        err && console.error(err);
-      });
-    });
-  });
+	.pipe(
+		// 返回合并后的目录
+		map((cur) => join(curFolder, cur)),
+		// 判断指定的目录是否已经存在
+		map((folder) => {
+			if (existsSync(folder)) return force ? folder : "";
+			mkdirSync(folder);
+			return folder;
+		})
+	)
+	.subscribe((folder) => {
+		// 根据返回的目录执行具体的写文件操作
+		if (!folder) {
+			console.log("folder exists");
+			return;
+		}
+		const curParserConfig = tsx
+			? parserConfig.filter((item) => ["tsx", "scss", "scss.d.ts"].includes(item.ext))
+			: parserConfig.filter((item) => ["js", "scss"].includes(item.ext));
+		// 根据配置文件里指定的文件后缀名以及模板内容，生成对应的文件
+		curParserConfig.forEach(({ ext, parser }) => {
+			writeFile(join(folder, `index.${ext}`), parser(basename(folder)), (err) => {
+				err && console.error(err);
+			});
+		});
+	});
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-03-03 16:47:23
  * @LastEditors: Huang canfeng
- * @LastEditTime: 2020-05-07 15:40:03
+ * @LastEditTime: 2020-08-04 22:33:06
  * @Description: 配置后缀名以及对应的解析方法，可以生成指定默认后缀名的模板文件
  */
 module.exports = [
@@ -47,5 +47,48 @@ module.exports = [
         N}}${
         N}`;
     }
-  }
+  },
+  {
+    ext: "scss.d.ts",
+    parser: () => { // 初始化Scss文件
+      const N = "\n";
+      return `declare namespace IndexScssNamespace {${
+        N} export interface IIndexScss {${
+        N}    con: string${
+        N}    img: string${
+        N}    txt: string${
+        N}  }${
+        N}}${
+        N}declare const IndexScssModule: IndexScssNamespace.IIndexScss & {${
+        N}  locals: IndexScssNamespace.IIndexScss;${
+        N}}${
+        N}export = IndexScssModule;${
+        N}`;
+    }
+  },
+  {
+    ext: "tsx",
+    parser: folder => { // 根据传入的目录名初始化Js文件
+      const date = new Date();
+      const N = "\n";
+      const createDate = date.toLocaleDateString("zh").replace(/\//g, "-");
+      const createTime = date.toLocaleTimeString("zh", { hour12: false });
+      const [firstChar, ...restChar] = folder || "";
+      const name = [String.prototype.toUpperCase.call(firstChar), ...restChar].join("");
+
+      return `/*${N} * @Date: ${createDate} ${createTime}${
+        N} * @LastEditors: Huang canfeng${
+        N} * @LastEditTime: ${createDate} ${createTime}${
+        N} * @Description:${
+        N} */${
+        N}import React, { FC, memo, useCallback, useRef, useEffect } from "react";${
+        N}import css from "./index.scss";${
+        N}${
+        N}const ${name}: FC = memo(props => {${
+        N}  return <></>;${
+        N}});${
+        N}export default ${name};${
+        N}`;
+    }
+  },
 ];
